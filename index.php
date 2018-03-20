3b00733884b7f61d01246339fd76f875fcd7ca33
Gaiji <34226052+Gaiji@users.noreply.github.com>
2018-03-20 17:16:43 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         }
     }
     define (URL, (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-    echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+    echo $_SERVER["REQUEST_URI"];
     br();
     echo URL;
     br();
