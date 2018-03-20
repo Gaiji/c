305ed8d94a8d46a5436c918c6050f6b0ed102139
Gaiji <34226052+Gaiji@users.noreply.github.com>
2018-03-20 16:56:14 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
+
 <?php
-	echo $_SERVER["REMOTE_ADDR"] ;
+	echo "サーバのIPアドレス:[{$_SERVER["SERVER_ADDR"]}]<br>";
+	$host=gethostbyaddr($_SERVER["SERVER_ADDR"]);
+	echo "サーバのホスト名:[{$host}]<br>";
+	echo "タイムスタンプ:[{$_SERVER["REQUEST_TIME''"]}]<br>";
+	echo "ACCEPT_CHARSET:[{$_SERVER["HTTP_ACCEPT_CHARSET"]}]<br>";
+	echo "HTTP_HOST:[{$_SERVER["HTTP_HOST"]}]<br>";
+	echo "HTTP_REFERER:[{$_SERVER["HTTP_REFERER"]}]<br>";
+	echo "HTTP_USER_AGENT:[{$_SERVER["HTTP_USER_AGENT"]}]<br>";
+	echo "IPアドレス:[{$_SERVER["REMOTE_ADDR"]}]<br>";
+	$host=gethostbyaddr($_SERVER["REMOTE_ADDR"]);
+	echo "ホスト名:[{$host}]<br>";
+	echo "ポート番号:[{$_SERVER["REMOTE_PORT"]}]<br>";
+?>
