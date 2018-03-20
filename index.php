80a6c7bfc3f68c2944167cabdb6cc1207757de93
Gaiji <34226052+Gaiji@users.noreply.github.com>
2018-03-20 17:30:05 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,15 @@
         }
     }
     define (URL, (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-    echo $_GET["name"];
+    if ($_GET["uuid"] == "5b222858-24d4-4060-ab61-55132d8042bc"){
+        $json_array = array(
+            'success' => true,
+            'ip' => getIPAddress(),
+        );
+        header("Content-Type: text/javascript; charset=utf-8");
+        echo json_encode($json_array);
+        return;
+    }
     br();
     echo URL;
     br();
